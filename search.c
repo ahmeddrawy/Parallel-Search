e09584c6367708666a5a1612f0ebc66c512c6f0b
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-04-09 22:36:52 +0200

    make one process calcualte and search all files

--- search.c
+++ search.c
@@ -27,6 +27,13 @@ int main(int argc , char * argv[]){
 		int l = 1 , u = 50 ;
 		MPI_Send(&l, 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
 		MPI_Send(&u, 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
+		int cnt = 0 ;
+		for(int i = 1 ; i < p ; ++i){
+			int tmpcnt = 0 ;
+			MPI_Recv(&tmpcnt, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+			cnt+=tmpcnt;
+		}
+		printf("%d\n", cnt);
 	}
 	else{ /// slave
 	//			MPI_Send( mat2[j], x, MPI_INT, (i)%(p-1) +1, tag, MPI_COMM_WORLD); ///sending matrix 2
@@ -36,13 +43,56 @@ int main(int argc , char * argv[]){
 		int lower_bound = 0 , upper_bound = -1;
 		MPI_Recv(&lower_bound, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
 		MPI_Recv(&upper_bound, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
-
+		char query[1024] =  "Energy";
+		char delim[] = " ";
+		int cnt =  0 ;
 		for(int i = lower_bound ; i <= upper_bound ; ++i){
-			char filename [80]; 
-			sprintf(filename,"Aristo-Mini-Corpus P-%d",i);
-			printf("%s\n",filename);
+			char filename [1024]; 
+			sprintf(filename,"Aristo-Mini-Corpus/Aristo-Mini-Corpus P-%d.txt",i);
+			// printf("%s\n",filename);
+			FILE * fp;
+		    char * line = NULL;
+		    size_t len = 0;
+		    ssize_t read;
+		    // strcat()
+		    fp = fopen(filename, "r");
+		    if (fp == NULL){
+		    	printf("failed");
+		        exit(EXIT_FAILURE);
+		    }
 
+		    while ((read = getline(&line, &len, fp)) != -1) {
+		    	/// now we have each line in the file we want to count on it 
+		    	char tmpquery[1024] ;
+		    	
+		    	strcpy(tmpquery , query);
+		    	char *ptr = strtok(tmpquery, delim); /// we divide the query to words 
+		    	int found = 1 ; 
+		    	while(ptr != NULL ){
+		    		char tmpline[2048];
+		    		//printf("%s\n" , ptr);
+		    		strcpy(tmpline , line);
+		    		printf("%s\n",tmpline);
+		    		if(strstr(tmpline, ptr) == NULL){
+		    			found = -1;
+		    		}
+		    		/// now we have a word from query we need to search all words from line to check if exist
+		    		ptr = strtok(NULL,delim);
+		    	}
+		    	if(found == 1){ /// we found all query words in this line
+		    		cnt++;
+		    		printf("%s\n",line);
+		    	}
+		    	
+		    }
+
+		    fclose(fp);
+		    if (line)
+		        free(line);
 		}
+		/// todo send cnt to root
+		MPI_Send( &cnt, 1, MPI_INT, 0, tag, MPI_COMM_WORLD); ///sending row of one
+		//printf("%d",cnt);	
 		
 	}
 
