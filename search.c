a8a38bffef8cb0fbf6fe0fcb15bb4bec7342349a
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-04-09 15:56:45 +0200

    getting file name using upper and lower bound for files range

--- search.c
+++ search.c
@@ -21,15 +21,28 @@ int main(int argc , char * argv[]){
 
 	/* Find out number of process */
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
-	// printf("%d\n",my_rank);
+	printf("%d\n",my_rank);
 
 	if( my_rank == 0){ /// master thread
-		
+		int l = 1 , u = 50 ;
+		MPI_Send(&l, 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
+		MPI_Send(&u, 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
 	}
 	else{ /// slave
 	//			MPI_Send( mat2[j], x, MPI_INT, (i)%(p-1) +1, tag, MPI_COMM_WORLD); ///sending matrix 2
 	//		MPI_Recv(row2, x, MPI_INT, (i)%(p-1) +1, tag, MPI_COMM_WORLD, &status);			
+		/// each slave recieve a lower and upper bound for the files numbers
+		/// and will iterate over them and find the matches
+		int lower_bound = 0 , upper_bound = -1;
+		MPI_Recv(&lower_bound, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+		MPI_Recv(&upper_bound, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
 
+		for(int i = lower_bound ; i <= upper_bound ; ++i){
+			char filename [80]; 
+			sprintf(filename,"Aristo-Mini-Corpus P-%d",i);
+			printf("%s\n",filename);
+
+		}
 		
 	}
 
